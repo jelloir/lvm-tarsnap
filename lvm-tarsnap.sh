b97425fcdbc653fa47bc3bb002ebcd5d8cc886ed
jelloir <j@mesrobertson.com>
2016-01-12 20:16:39 +1100

    Change commments, add backup function returns, edit error function

--- lvm-tarsnap.sh
+++ lvm-tarsnap.sh
@@ -9,73 +9,57 @@ lvsnap_suffix="-snapshot"
 
 lvsnap_create()
 # create lvm snapshot
-# $1 = volume group (v)
-# $2 = logical volume (l)
-# $3 = lv snapshot suffix (x)
-# $4 = lv size in % (z)
 {
-    local v=$1; shift
-    local l=$1; shift
-    local x=$1; shift
-    local z=$1; shift
+    local v=$1; shift # $1 volume group (v)
+    local l=$1; shift # $2 logical volume (l)
+    local x=$1; shift # $3 lv snapshot suffix (x)
+    local z=$1; shift # $4 lv size in % (z)
     lvcreate -s -l +$z%FREE -n $l$x $v/$l && return 0 || return 1
 }
 
 lvsnap_remove()
 # remove lvm snapshot
-# $1 = volume group (v)
-# $2 = logical volume (l)
-# $3 = lv snapshot suffix (x)
 {
-    local v=$1; shift
-    local l=$1; shift
-    local x=$1; shift
+    local v=$1; shift # $1 volume group (v)
+    local l=$1; shift # $2 logical volume (l)
+    local x=$1; shift # $3 lv snapshot suffix (x)
     lvremove --force $v/$l$x && return 0 || return 1
 }
 
 lvsnap_mount()
 # mount lvm snapshot
-# $1 = volume group (v)
-# $2 = logical volume (l)
-# $3 = lv snapshot suffix (x)
-# $4 = mount options (m)
 {
-    local v=$1; shift
-    local l=$1; shift
-    local x=$1; shift
-    local m=$1; shift
+    local v=$1; shift # $1 volume group (v)
+    local l=$1; shift # $2 logical volume (l)
+    local x=$1; shift # $3 lv snapshot suffix (x)
+    local m=$1; shift # $4 mount options (m)
     test -d /mnt/$l$x || mkdir -p /mnt/$l$x
     mount -o $m /dev/$v/$l$x /mnt/$l$x && return 0 || return 1
 }
 
 lvsnap_umount()
 # un-mount lvm snapshot
-# $1 = logical volume (l)
-# $2 = lv snapshot suffix (x)
 {
-    local l=$1; shift
-    local x=$1; shift
+    local l=$1; shift # $2 logical volume (l)
+    local x=$1; shift # $2 lv snapshot suffix (x)
     umount /mnt/$l$x && return 0 || return 1
 }
 
 backup()
 # run tarsnap backup
-# $1 = logical volume (l)
-# $2 = lv snapshot suffix (x)
-# $3 = exclusions
 {
-    local l=$1; shift
-    local x=$1; shift
-    local c="$(IFS=, ; for e in $1 ; do printf "%s" "--exclude=\"$e\" " ; done)"; shift
-    eval tarsnap -C /mnt/$l$x -c -f $l-`date +%Y-%m-%d_%H-%M-%S` $c ./
-    # need to integrade return functions
+    local l=$1; shift # $1 logical volume (l)
+    local x=$1; shift # $2 lv snapshot suffix (x)
+    local c="$(IFS=, ; for e in $1 ; do printf "%s" "--exclude=\"$e\" " ; done)"; shift # $3 exclusions
+    eval tarsnap -C /mnt/$l$x -c -f $l-`date +%Y-%m-%d_%H-%M-%S` $c ./ \
+    && return 0 || return 1
 }
 
 error()
 # report errors
 {
     local f=$1; shift
-    retur
+    return "error in step $f"
 }
 
 main()
